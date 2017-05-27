cc1bc113d13148405403cec04e26c9b5366a0c02
Centro Ático <centroatico@202C11E18168974.local>
2017-05-27 12:48:27 -0500

    Registro funcional

--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -1,25 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from instagramapp.models import *
 # Create your views here.
 
 def index(request):
     return render(request, 'registro.html')
 def crear_usuario(request):
-    print ("Entre")
-    email = request.POST['email']
-    username = request.POST['username']
-    password = request.POST['password']
-    name = request.POST['name']
-    print (email)
-    print (username)
-    print (password)
-    print (name)
-    return render(request, 'registro.html')
+    _email = request.POST['email']
+    _username = request.POST['username']
+    _password = request.POST['password']
+    _name = request.POST['name']
+    print (_email)
+    print (_username)
+    print (_password)
+    print (_name)
+    user = User.objects.create_user( username = _username , email = _email , first_name = _name , password = _password )
+    myuser = MiUsuario( usuario = user )
+    myuser.save()
+    print (user)
+    print (User.password)
+    return redirect('login')
 
 def login(request):
     return render(request, 'login.html')
 def home(request):
-    return render(request, 'home2.html')
+    return render(request, 'home.html')
 def perfil(request):
     return render(request, 'perfil.html')
 def buscar(request):
